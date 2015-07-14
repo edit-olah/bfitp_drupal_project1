e8c905ccd40ce7121193935bd19ca87a5ffd03db
Edit Olah <sudoeditadm@gmail.com>
2015-07-14 18:07:51 +0100

    adding multiple node load to pull in content for callout boxes - home_page content type template

--- sites/all/themes/bfitp/templates/nodes/node--home_page.tpl.php
+++ sites/all/themes/bfitp/templates/nodes/node--home_page.tpl.php
@@ -1,7 +1,7 @@
 <main>
     <div id="MainContent">
 
-	<!-- HERO Panel/page beginning -->
+	<!-- HERO Panel beginning -->
 	<div id="Hero">
 
 	    <div class="heroContent" id="home"
@@ -12,8 +12,8 @@
 		<div class="container">
 		    <div class="heroText">
 			<section>
-			    <?php if(isset($field_hero_title[und][0]['value'])){
-				print '<h1>' . $field_hero_title[und][0]['value'] . '</h1>';
+			    <?php if(isset($field_hero_title['und'][0]['value'])){
+				print '<h1>' . $field_hero_title['und'][0]['value'] . '</h1>';
 			    } ?>
 			    <?php if(isset($field_tagline[0]['value'])){
 				print '<h2 class="tagline">' . $field_tagline[0]['value'] . '</h2>';
@@ -28,10 +28,10 @@
 
 	</div><!-- / #Hero -->
 
-	<!-- HERO Panel/page end -->
+	<!-- HERO Panel end -->
 
 
-	<!-- ABOUT US PAGE beginning -->
+	<!-- ABOUT US SECTION beginning -->
 
 	<div class="page" id="About-us">
 
@@ -47,9 +47,9 @@
 
 	</div> <!-- /.page#About-us -->
 
-	<!-- ABOUT US PAGE end -->
+	<!-- ABOUT US SECTION end -->
 
-	<!-- HOME PORTFOLIO PAGE beginning -->
+	<!-- HOME CALLOUT BOXES SECTION beginning -->
 
 	<div class="page" id="HomePortfolio">
 
@@ -57,53 +57,74 @@
 
 		<ul class="grid">
 		    
-		    <?php
-		    if(isset($field_main_content_block_ref['und'][0])){
-			foreach($field_main_content_block_ref['und'] as $block){
-			    $delta = explode(":", $block['moddelta']);
-			    $whole_block = block_load($delta[0],$delta[1]);
-			    $block_to_render = _block_get_renderable_array(_block_render_blocks(array($whole_block)));
-			    print drupal_render($block_to_render);
+		    <?php if(isset($field_callout_boxes[0]['target_id'])){
+			// create an array
+			$nodesToLoad = array();
+			
+			//get the target_id for all nodes that we want to load 
+			//here and put it into an array, where we don't specify a 
+			//key-value pair, so it just adds the value to the next 
+			//available slot
+			foreach($field_callout_boxes as $callout_box){
+			    $nodesToLoad[] = $callout_box['target_id'];
 			}
-		    } ?>
+			
+			//load all contents of multiple nodes using the array 
+			//we created above and put all that into a variable
+			$contacts = node_load_multiple($nodesToLoad);
+			
+			//for each node that we have the content for in the 
+			//$contacts variable, put the content into a variable 
+			//called $contact and print out the followings 
+			foreach ($contacts as $contact){
+			    ?>
+			    <li class="click-follow">
+				    <div class="grid-contentHolder">
+					<div class="leftContainer">
+					    <?php if(isset($contact->field_contact_me_profile_image)){
+						print '<img alt="' . $contact->field_contact_me_profile_image['und'][0]['alt'] . 
+							'" alt="' . $contact->field_contact_me_profile_image['und'][0]['title'] . 
+							'" src="' . image_style_url('general_square', $contact->field_contact_me_profile_image['und'][0]['uri']) .'" />';
+					    } ?>
+					</div>
+					<div class="rightContainer">
+					    <section>
 
-		    <li class="click-follow">
-			<div class="grid-contentHolder">
-			    <div class="leftContainer">
-				<img alt="" src="images/bfitp_placeholder_img_300x300.png" >
-			    </div>
-			    <div class="rightContainer">
-				<section>
-				    <h2>Peter Whyte</h2>
-				    <h3>Mid-weight web developer</h3>
-				    <p class="button"><a href="/peter-whyte.html">Go to Peter's portfolio >></a></p>
-				</section>
-			    </div>
+						<?php if(isset($contact->field_contact_highlighted_text['und'][0]['value']) && 
+							isset($contact->field_contact_highlighted_text_2['und'][0]['value'])){
+							    print '<h2>' . $contact->field_contact_highlighted_text['und'][0]['value'] 
+								    . ' ' . $contact->field_contact_highlighted_text_2['und'][0]['value']
+								    . '</h2>';
+						}?>
+						<?php if(isset($contact->field_contact_highlighted_text_3['und'][0]['value'])){
+						    print '<h3>' 
+							    . $contact->field_contact_highlighted_text_3['und'][0]['value'] 
+							. '</h3>';
+						}?>
+						<?php if(isset($contact->field_contact_highlighted_text['und'][0]['value']) 
+						    && isset($contact->field_contact_highlighted_text_2['und'][0]['value'])){
+							print '<p class="button">' 
+								. '<a href="' . '/' . $contact->field_contact_highlighted_text['und'][0]['value'] . '-' . $contact->field_contact_highlighted_text_2['und'][0]['value'] . '">' 
+								    . 'Go to ' . $contact->field_contact_highlighted_text['und'][0]['value'] . '\'s portfolio >>'
+								. '</a>' 
+							    . '</p>';
+						}?>
+					    </section>
+					</div>
 
-			</div><!-- /.grid-contentHolder -->
-		    </li>
-		    <li class="click-follow">
-			<div class="grid-contentHolder">
-			    <div class="leftContainer">
-				<img alt="" src="images/bfitp_placeholder_img_300x300.png" >
-			    </div>
-			    <div class="rightContainer">
-				<section>
-				    <h2>Edit Olah</h2>
-				    <h3>Junior web developer</h3>
-				    <p class="button"><a href="/edit-olah.html">Go to Edit's portfolio >></a></p>
-				</section>
-			    </div>
-
-			</div><!-- /.grid-contentHolder -->
-		    </li>
+				    </div><!-- /.grid-contentHolder -->
+				</li>
+			    <?php } ?>
+		    <?php } ?>
+		    
+		    
 		</ul><!-- /.grid -->
 
 	    </div><!-- /.container -->
 
 	</div> <!-- /.page#HomePortfolio -->
 
-	<!-- HOME PORTFOLIO PAGE end -->                  
+	<!-- HOME CALLOUT BOXES SECTION end -->                  
 
 
 
